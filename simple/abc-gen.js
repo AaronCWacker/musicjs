c9e5ca55abfe8a455d044ab4ffdfc6896dda33b2
John Allsup <dev@chalisque.net>
2020-10-24 18:38:54 +0100

    Factored some of the classes from cs.html into separate files

---
+++ simple/abc-gen.js
@@ -0,0 +1,26 @@
+class AbcGenerator {
+  constructor(rng,ui,paper) {
+    console.log("abc constructor")
+    this.rng = rng
+    this.ui = ui
+    this.paper = paper
+    this.ui.add_callback(this.generate)
+    this.generate()
+  }
+  generate = () => {
+    // leave this method alone in subclasses
+    const abc = this.generate_abc()
+    ABCJS.renderAbc(this.paper,abc,{responsive:"resize"})
+  }
+  generate_abc() {
+    // override this method in subclasses
+    const sample_abc = `
+X:1
+M:12/8
+L:1/8
+K:clef=bass
+CCCC CCCC|DDDD DEFG|]      
+    `
+    return sample_abc
+  }
+}
