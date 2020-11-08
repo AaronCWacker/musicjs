92f0e852675faac2a3863b6e220041529256f88a
John Allsup <dev@chalisque.net>
2020-11-08 12:13:16 +0000

    Added index.php to repo

--- expt3/index.php
+++ expt3/index.php
@@ -6,13 +6,16 @@
   <title>Chord drills</title>
 </head>
 <body>
-  <ul>
+<h1>Random music practice drills</h1>
+<p>Here are a few things I wrote for piano practice. Basically it takes a simple pattern and gives you random notes within that pattern. The intent is to aid composition and improvisation by giving you patterns to practice to, covering possibilities you'd otherwise miss.</p>
+<p>It uses a random number generator that takes seed values (<a href='https://github.com/DomenicoDeFelice/jsrand'>from here</a>) and the controls at the top control the seed. The Daily button takes the number of days from 1/1/2020, multiplied by 100 to generate a seed. The +1/-1 etc. buttons select different seeds. These are mapped to X,C,V,B,N,M. Z is mapped to Zero. S toggles sharps/flats. The point of the seed is that this can reproduce exactly a pattern you've seen before, all you need to remember is the seed.</p>
+<ul>
 <?php
 $g = glob("*.html");
 foreach($g as $a) {
   echo "<li><a href='$a'>$a</a></li>\n";
 }
 ?>
-  </ul>
+</ul>
 </body>
 </html>
